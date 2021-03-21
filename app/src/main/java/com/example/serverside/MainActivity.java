b57bea5d301c5f50b2d642a028d5c4220f9f4a4d
SRS Sohan <srs1059@gmail.com>
2021-03-21 17:07:43 +0600

    done

--- app/src/main/java/com/example/serverside/MainActivity.java
+++ app/src/main/java/com/example/serverside/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.serverside;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -21,23 +28,45 @@ public class MainActivity extends AppCompatActivity {
     InputStreamReader inputStreamReader;
     BufferedReader bufferedReader;
     String msg;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = findViewById(R.id.img);
         Thread thread = new Thread() {
             public void run()
             {
                 try {
-                    serverSocket = new ServerSocket();
+                    serverSocket = new ServerSocket(8080);
                     while (true)
                     {
                         s = serverSocket.accept();
-                        inputStreamReader = new InputStreamReader(s.getInputStream());
+                        /*inputStreamReader = new InputStreamReader(s.getInputStream());
                         bufferedReader = new BufferedReader(inputStreamReader);
                         msg = bufferedReader.readLine();
-                        Log.d("message", msg);
+                        //Log.d("message", String.valueOf(msg.length()));
+
+                         */
+                        DataInputStream input;
+                        InputStream in = s.getInputStream();
+                        input = new DataInputStream(in);
+                        byte[] data;//String read = input.readLine();
+                        int len= input.readInt();
+                        data = new byte[len];
+                        if (len > 0) {
+                            input.readFully(data,0,data.length);
+                        }
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data.length);
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -49,6 +78,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public static Bitmap stringToImage(String string)
+    {
+        byte[] imgBytes= Base64.decode(string, Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(imgBytes, 0, imgBytes.length);
+
+        return bitmap;
+    }
+
+    public static String imageToString(Bitmap bitmap)
+    {
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        float aspectRatio = bitmap.getWidth() / (float)bitmap.getHeight();
+        int width = 1280;
+        int height = Math.round(width / aspectRatio);
+        bitmap = Bitmap.createScaledBitmap(bitmap,width,height,false);
+        bitmap.compress(Bitmap.CompressFormat.JPEG,30,byteArrayOutputStream);
+        byte[] imgBytes=byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgBytes, Base64.DEFAULT);
+    }
+
+
 
 
 }
